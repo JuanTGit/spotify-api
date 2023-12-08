d0735c391d3b223f8b02e2874f4692d2975c41de
Juan <j.tejeda46@yahoo.com>
2023-12-08 01:11:00 -0600

    Added function to fetch api response and return data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('js file connected')
+// Declaration for song values;
+let song;
+let playsong;
+
+// Spotify Credentials;
+
+const clientId = 'd0a4d78b5eac429abee175f6ff8ccd57'
+const clientSecret = 'CLIENT SECRET KEY'
+
+// Fuction to get token from Spotify
+
+const getToken = async () => {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+        },
+        body: 'grant_type=client_credentials'
+    });
+    const data = await response.json()
+    return data.access_token
+}
